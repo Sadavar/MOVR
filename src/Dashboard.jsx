41ac8b341bb0865c0fba2cbf59cbace7a95a1127
Varun Sadasivam <sadavar05@gmail.com>
2024-01-20 13:44:24 -0500

    skeleton

---
+++ src/Dashboard.jsx
@@ -0,0 +1,13 @@
+import Map from './Map';
+
+export default function Dashboard() {
+    return (
+        <>
+            <div className="Dashboard">
+
+                <Map />
+
+            </div>
+        </>
+    )
+}
